320e55081fecd0fa4dc7bdd3e83d074bb19e84d4
Ubuntu <ubuntu@ip-172-31-17-252.eu-north-1.compute.internal>
2025-06-19 07:04:14 +0000

    Add CloudWatch dashboard for ECS monitoring

--- iam.tf
+++ iam.tf
@@ -1,17 +1,17 @@
-# terraform/iam.tf
-
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.project_name}-task-exec-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 
   tags = {
@@ -23,3 +23,32 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_policy" "ecs_cloudwatch_logs_policy" {
+  name        = "${var.project_name}-cloudwatch-logs-policy"
+  description = "Allows ECS tasks to send logs and publish metrics to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "cloudwatch:PutMetricData",
+          "ec2:DescribeTags",
+          "ecs:ListTasks",
+          "ecs:DescribeTasks"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_cloudwatch_logs_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_cloudwatch_logs_policy.arn
+}
+
