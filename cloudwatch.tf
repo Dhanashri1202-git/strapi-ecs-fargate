f7d36c786c63e449d03e0413ee324a917fb9e307
Ubuntu <ubuntu@ip-172-31-10-74.ap-south-1.compute.internal>
2025-06-21 13:42:15 +0000

    Fix invalid syntax in cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_group" "strapi" {
     Name = "${var.project_name}-log-group"
   }
 }
-1~resource "aws_cloudwatch_log_group" "strapi" {
+resource "aws_cloudwatch_log_group" "strapi" {
   name              = "/ecs/${var.project_name}"
   retention_in_days = 7
 
