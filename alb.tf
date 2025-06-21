612e8ed5b4cc41690a3ba84a04cebd1c6c5b55a4
Ubuntu <ubuntu@ip-172-31-10-74.ap-south-1.compute.internal>
2025-06-21 15:13:11 +0000

    solving the error

--- alb.tf
+++ alb.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "strapi" {
   target_type  = "ip"
 
   health_check {
-    path                = "/admin"
+    path                = "/"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
